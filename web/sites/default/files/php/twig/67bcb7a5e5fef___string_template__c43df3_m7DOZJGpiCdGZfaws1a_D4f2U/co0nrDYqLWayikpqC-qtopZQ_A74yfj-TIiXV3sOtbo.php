903d25c4805d71b14cad9401e7e35cfc8da3f990
afaqraza16 <afaqraza0220@gmail.com>
2025-02-26 15:00:43 +0500

    i have added some extra functionlity by  installing some modulues

---
+++ web/sites/default/files/php/twig/67bcb7a5e5fef___string_template__c43df3_m7DOZJGpiCdGZfaws1a_D4f2U/co0nrDYqLWayikpqC-qtopZQ_A74yfj-TIiXV3sOtbo.php
@@ -0,0 +1,109 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* __string_template__c43df3039a1fa48e29f6592251999cb7 */
+class __TwigTemplate_60d6d1afa1693ec60ee6640ec436e9ed extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->extensions[SandboxExtension::class];
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 1
+        yield "<div class=\"contextual-region media media--type-image media--view-mode-default\">
+  <div data-contextual-id=\"media:media=2:changed=1740511045&amp;langcode=en\" data-contextual-token=\"J1W2zr3cVnAbk-RAdyKjbkOprfU9YK51ZxieteZ4Qbk\" data-drupal-ajax-container></div>
+      
+  <div class=\"field field--name-field-media-image field--type-image field--label-visually_hidden\">
+    <div class=\"field__label visually-hidden\"><a href=\"https://drupal1.ddev.site/misson\" target=\"_blank\">Image</a></div>
+              <div class=\"field__item\">  <a href=\"https://drupal1.ddev.site/misson\" target=\"_blank\"><img loading=\"lazy\" src=\"/sites/default/files/styles/large/public/2025-02/fig1.png.webp?itok=sWcnl36x\" width=\"480\" height=\"402\" alt=\"Drupaks\"></a>
+
+
+</div>
+          </div>
+
+  </div>
+";
+        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "__string_template__c43df3039a1fa48e29f6592251999cb7";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  44 => 1,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("", "__string_template__c43df3039a1fa48e29f6592251999cb7", "");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = [];
+        static $filters = [];
+        static $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                [],
+                [],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
